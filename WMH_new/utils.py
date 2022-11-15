742cc3982cc5f986e4b5ba37ba09e2384bb2ca17
Matthijs Luijten <matthijs.luijten@hotmail.com>
2022-11-15 12:29:51 +0100

    add function to add case to training or test data

--- WMH_new/utils.py
+++ WMH_new/utils.py
@@ -1,5 +1,6 @@
 from keras import backend as K
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def iou_coef(y_true, y_pred):
@@ -41,3 +42,77 @@ def get_crop_shape(target, refer):
         ch1, ch2 = int(ch/2), int(ch/2)
 
     return (ch1, ch2), (cw1, cw2)
+
+
+def determine_training(f1, l1, f2, l2, f3, l3, case=''):
+    with open('WMH_new/training_cases.txt') as f:
+        if case in f.read():
+            return
+    with open('WMH_new/test_cases.txt') as f:
+        if case in f.read():
+            return
+    plt.ion()
+    fig, ax = plt.subplots(3,2, figsize=(10,6))
+    fig.suptitle(case)
+    ax[0,0].imshow(np.rot90(f1), cmap='gray')
+    ax[0,0].set_title('Original FL')
+    ax[0,0].axis('off')
+
+    ax[0,1].imshow(np.rot90(f1), cmap='gray')
+    ax[0,1].set_title('FL and LBL')
+    ax[0,1].axis('off')
+
+    # ax[0].imshow(np.rot90(label), cmap='hot', alpha=0.5)
+    # ax[0].set_title('WMH label')
+    # ax[0].axis('off')
+
+    ax[0,1].imshow(np.rot90(l1), cmap='hot', alpha=0.5)
+    # ax[1].set_title('WMH label')
+    ax[0,1].axis('off')
+
+    ax[1,0].imshow(np.rot90(f2), cmap='gray')
+    ax[1,0].set_title('Original FL')
+    ax[1,0].axis('off')
+
+    ax[1,1].imshow(np.rot90(f2), cmap='gray')
+    ax[1,1].set_title('FL and LBL')
+    ax[1,1].axis('off')
+
+    ax[1,1].imshow(np.rot90(l2), cmap='hot', alpha=0.5)
+    # ax[1].set_title('WMH label')
+    ax[1,1].axis('off')
+
+    ax[2,0].imshow(np.rot90(f3), cmap='gray')
+    ax[2,0].set_title('Original FL')
+    ax[2,0].axis('off')
+
+    ax[2,1].imshow(np.rot90(f3), cmap='gray')
+    ax[2,1].set_title('FL and LBL')
+    ax[2,1].axis('off')
+
+    ax[2,1].imshow(np.rot90(l3), cmap='hot', alpha=0.5)
+    # ax[1].set_title('WMH label')
+    ax[2,1].axis('off')
+
+    plt.show()
+
+    while True:
+        user_input = input('\n Use this as training data? (y/n): ')
+        if user_input.lower() == 'y':
+            print(f'✓ {case}')
+            with open('WMH_new/training_cases.txt', 'a') as f:
+                f.write(case + '\n') 
+                f.close()
+            break
+        elif user_input.lower() == 'n':
+            print(f'✗ {case}')
+            with open('WMH_new/test_cases.txt', 'a') as f:
+                f.write(case + '\n') 
+                f.close()
+            break
+        else:
+            print('Type \'y\' or \'n\'')
+            continue
+
+
+    plt.close()
